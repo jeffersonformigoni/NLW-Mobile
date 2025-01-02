33fbe9456fb0afc2a39b360dfab9a0954c566dac
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-01-02 16:58:30 -0300

    Add todos os titles e subtitles falta o botão da aplicação

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,10 @@ import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
 
 export default function Index() {
-    return (
-        <View style={{ flex: 1, padding: 40, gap: 40 }}>
-            <Welcome />
-            <Steps />
-        </View>
-    )
+  return (
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+    </View>
+  )
 }
--- src/components/welcome/index.tsx
+++ src/components/welcome/index.tsx
@@ -3,15 +3,15 @@ import { Image, Text, View } from "react-native"
 import { s } from "./styles"
 
 export function Welcome() {
-    return (
-        <View>
-            <Image source={require("@/assets/logo.png")} style={s.logo} />
+  return (
+    <View>
+      <Image source={require("@/assets/logo.png")} style={s.logo} />
 
-            <Text style={s.title}>Boas vindas ao Nearby!</Text>
+      <Text style={s.title}>Boas vindas ao Nearby!</Text>
 
-            <Text style={s.subtitle}>
-                Tenhas cupons de vantagens para usar{'\n'}em seus estabelecimentos favoritos.
-            </Text>
-        </View>
-    )
+      <Text style={s.subtitle}>
+        Tenha cupons de vantagem para usar em&nbsp;seus estabelecimentos favoritos.
+      </Text>
+    </View>
+  )
 }

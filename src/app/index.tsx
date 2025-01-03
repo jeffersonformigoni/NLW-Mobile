f8f7d1f3c78fe0dbc62c68ef0e50bec950752dd0
Jefferson Formigoni Araujo <jeffersonformigoni86@gmail.com>
2025-01-03 20:31:32 -0300

    Add page home e API para ser consumida pelo APP não fui eu que criei a API apenas usando para ser consumida como parte do exercicío

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { View, Text } from "react-native"
+import { router } from "expo-router"
 
 import { Steps } from "@/components/steps"
 import { Button } from "@/components/button" 
@@ -10,7 +11,7 @@ export default function Index() {
       <Welcome />
       <Steps />
 
-      <Button isLoading={false}>
+      <Button onPress={() => router.navigate("/home")}>
         <Button.Title>Começar</Button.Title>
       </Button>
     </View>
